2d30a2d7a2829fa7e987f235de893f3f6d1d9676
TheSlowGrowth <9356320+TheSlowGrowth@users.noreply.github.com>
2021-11-28 21:58:46 +0100

    allow multiple types in LateInitializedObject

--- firmware/src/util/LateInitializedObject.h
+++ firmware/src/util/LateInitializedObject.h
@@ -1,16 +1,16 @@
-/**	
+/**
  * Copyright (C) Johannes Elliesen, 2021
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * any later version.
- *  
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
@@ -20,27 +20,84 @@
 #include <cstddef>
 #include <utility>
 
-template <typename T>
+template <class... Types>
 class LateInitializedObject
 {
+    template <typename T, typename... AllTypes>
+    struct isOneOf : public std::false_type
+    {
+    };
+    template <typename T, typename FirstType, typename... RestOfTypes>
+    struct isOneOf<T, FirstType, RestOfTypes...> :
+        public std::conditional<std::is_same<T, FirstType>::value,
+                                std::true_type,
+                                isOneOf<T, RestOfTypes...>>::type
+    {
+    };
+
+    template <typename... Ts>
+    using FirstTypeOf =
+        typename std::tuple_element<0, std::tuple<Ts...>>::type;
+
 public:
     LateInitializedObject() :
         object_(nullptr)
     {
     }
 
-    template <typename... Args>
-    void create(Args&&... args)
+    template <class T = FirstTypeOf<Types...>,
+              typename... Args,
+              typename std::enable_if<isOneOf<T, Types...>::value>::type* = nullptr>
+    T* create(Args&&... args)
     {
         object_ = new (::std::addressof(storage_)) T(std::forward<Args>(args)...);
+        return reinterpret_cast<T*>(object_);
     }
 
-    operator T*() { return object_; }
-    operator const T*() const { return object_; }
-    T* operator->() { return object_; }
-    const T* operator->() const { return object_; }
+    template <class T, typename std::enable_if<isOneOf<T, Types...>::value>::type* = nullptr>
+    void destroy()
+    {
+        if (object_)
+        {
+            // manually call destructor - no delete since we used placement new()
+            reinterpret_cast<T*>(object_)->~T();
+            object_ = nullptr;
+        }
+    }
+
+    template <class T, typename std::enable_if<isOneOf<T, Types...>::value>::type* = nullptr>
+    T& as()
+    {
+        return *reinterpret_cast<T*>(object_);
+    }
+
+    template <class T, typename std::enable_if<isOneOf<T, Types...>::value>::type* = nullptr>
+    const T& as() const
+    {
+        return *reinterpret_cast<T*>(object_);
+    }
+
+    /** operator->() for the first type in `Types` */
+    FirstTypeOf<Types...>* operator->()
+    {
+        return reinterpret_cast<FirstTypeOf<Types...>*>(object_);
+    }
+    const FirstTypeOf<Types...>* operator->() const
+    {
+        return reinterpret_cast<FirstTypeOf<Types...>*>(object_);
+    }
+
+    /** conversion operator for the first type in `Types` */
+    operator FirstTypeOf<Types...>&()
+    {
+        return *reinterpret_cast<FirstTypeOf<Types...>*>(object_);
+    }
+    operator const FirstTypeOf<Types...>&() const
+    {
+        return *reinterpret_cast<FirstTypeOf<Types...>*>(object_);
+    }
 
 private:
-    typename std::aligned_storage<sizeof(T)>::type storage_;
-    T* object_;
+    typename std::aligned_union<0, Types...>::type storage_;
+    void* object_;
 };
