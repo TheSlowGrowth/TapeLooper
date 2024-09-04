082268e8738462ca882c4bd0e989e34ae2d046cb
TheSlowGrowth <9356320+TheSlowGrowth@users.noreply.github.com>
2024-09-04 18:35:03 +0200

    chore: add pre-commit hook for clang-format

---
+++ scripts/setup-clangformat-precommit-hook.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# run this script from the repository root to install the pre-commit
+# hook that will check formatting before you commit.
+
+pip install pre-commit
+pre-commit install
